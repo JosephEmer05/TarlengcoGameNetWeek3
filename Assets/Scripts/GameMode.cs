0153b8653d577238fc9cfb8f2b803ca3244fe24a
JosephEmer05 <ej.tarlengco@gmail.com>
2026-01-27 12:02:02 +0800

    ..

--- Assets/Scripts/GameMode.cs
+++ Assets/Scripts/GameMode.cs
@@ -1,78 +1,85 @@
 using UnityEngine;
 using Fusion;
+using TMPro;
 
 public class NetworkPlayer : NetworkBehaviour
 {
+    [Header("Visuals")]
     [SerializeField] private MeshRenderer _meshRenderer;
-    #region Fusion Callbacks
-    //relevant to the network, do it in spawned (initialization)
+    [SerializeField] private TextMeshPro _nameLabel;
 
     [Header("Networked Properties")]
-
-    [Networked]  public Vector3 NetworkedPosition { get; set; }
+    [Networked] public Vector3 NetworkedPosition { get; set; }
     [Networked] public Color PlayerColor { get; set; }
+    [Networked] public NetworkString<_16> PlayerName { get; set; }
 
     public override void Spawned()
     {
-        if (HasInputAuthority)
-        {
-
-        }
-
         if (HasStateAuthority)
         {
-            PlayerColor = Random.ColorHSV();
+            NetworkedPosition = transform.position;
+        }
+        UpdateName();
+        UpdateColor();
+        if (HasInputAuthority)
+        {
+            string myName = NetworkManager.LocalPlayerName;
+            Color myColor = NetworkManager.LocalPlayerColor;
+            RPC_SetPlayerStats(myName, myColor);
         }
     }
 
-    //On destroy
-    public override void Despawned(NetworkRunner runner, bool hasState)
-    {
-
-    }
-
-    //update function
     public override void FixedUpdateNetwork()
     {
-        if(GetInput(out NetworkInputData input))
+        if (GetInput(out NetworkInputData input))
         {
-            this.transform.position += 
-                new Vector3(input.InputVector.normalized.x, input.InputVector.normalized.y) 
-                * Runner.DeltaTime;
-
-            NetworkedPosition = this.transform.position;
+            Vector3 direction = new Vector3(input.InputVector.x, 0, input.InputVector.y);
+            transform.position += direction * Runner.DeltaTime * 5f;
+            NetworkedPosition = transform.position;
         }
     }
 
-    //happens after fixedupdatenetwork, for nonserver objects
     public override void Render()
     {
-        this.transform.position = NetworkedPosition;
+        transform.position = Vector3.Lerp(transform.position, NetworkedPosition, Time.deltaTime * 10f);
+
         if (_meshRenderer != null && _meshRenderer.material.color != PlayerColor)
         {
             _meshRenderer.material.color = PlayerColor;
         }
+
+        if (_nameLabel != null && _nameLabel.text != PlayerName.ToString())
+        {
+            _nameLabel.text = PlayerName.ToString();
+        }
+
+        if (HasInputAuthority)
+        {
+            Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y + 10, transform.position.z - 10);
+            Camera.main.transform.LookAt(transform.position);
+        }
     }
 
     [Rpc(RpcSources.InputAuthority, RpcTargets.StateAuthority)]
-    private void RPC_SetPlayerColor(Color color)
+    private void RPC_SetPlayerStats(string name, Color color)
     {
-        if (HasStateAuthority)
-        {
-            this.PlayerColor = color;
-        }
+        this.PlayerName = name;
+        this.PlayerColor = color;
     }
-    #endregion
 
-    #region Unity Callbacks
-    private void Update()
+    private void UpdateName()
     {
-        if (!HasInputAuthority) return;
-        if (Input.GetKeyDown(KeyCode.Q))
+        if (_nameLabel != null)
         {
-            var randColor = Random.ColorHSV();
-            RPC_SetPlayerColor(randColor);
+            _nameLabel.text = PlayerName.ToString();
         }
     }
-    #endregion
+
+    private void UpdateColor()
+    {
+        if (_meshRenderer != null)
+        {
+            _meshRenderer.material.color = PlayerColor;
+        }
+    }
 }
--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -4,20 +4,48 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+
 public class NetworkManager : MonoBehaviour, INetworkRunnerCallbacks
 {
     #region Public Variables
+    [Header("Spawning")]
     [SerializeField] private NetworkPrefabRef playerPrefab;
+
+    [Header("UI References")]
+    [SerializeField] private TMP_InputField nameInput;
+    [SerializeField] private TMP_Dropdown colorDropdown;
+    [SerializeField] private GameObject uiPanel;
     #endregion
 
     #region Private Variables
-    private Dictionary<PlayerRef, NetworkObject> _spawnedCharacters = null;
+    private Dictionary<PlayerRef, NetworkObject> _spawnedCharacters = new Dictionary<PlayerRef, NetworkObject>();
     private NetworkRunner _networkRunner;
     #endregion
 
-    async void StartGame(GameMode game)
+    public static string LocalPlayerName;
+    public static Color LocalPlayerColor;
+
+    public void OnJoinGameClicked()
     {
-        _networkRunner = this.gameObject.AddComponent<NetworkRunner>();
+        LocalPlayerName = nameInput.text;
+        if (string.IsNullOrEmpty(LocalPlayerName)) LocalPlayerName = "Player";
+
+        switch (colorDropdown.value)
+        {
+            case 0: LocalPlayerColor = Color.red; break;
+            case 1: LocalPlayerColor = Color.green; break;
+            case 2: LocalPlayerColor = Color.blue; break;
+            default: LocalPlayerColor = Color.white; break;
+        }
+
+        if (uiPanel != null) uiPanel.SetActive(false);
+        StartGame();
+    }
+
+    async void StartGame()
+    {
+        _networkRunner = gameObject.AddComponent<NetworkRunner>();
         _networkRunner.ProvideInput = true;
 
         var scene = SceneRef.FromIndex(SceneManager.GetActiveScene().buildIndex);
@@ -28,129 +56,55 @@ public class NetworkManager : MonoBehaviour, INetworkRunnerCallbacks
 
         await _networkRunner.StartGame(new StartGameArgs()
         {
-            GameMode = game,
+            GameMode = GameMode.AutoHostOrClient,
             SessionName = "TestSession",
             Scene = scene,
             SceneManager = gameObject.AddComponent<NetworkSceneManagerDefault>()
         });
     }
 
-    private void Start()
-    {
-        #if SERVER
-        StartGame(GameMode.Host);
-        #elif CLIENT
-        StartGame(GameMode.Client);
-        #endif
-    }
-
-    public void OnInput(NetworkRunner runner, NetworkInput input)
-    {
-        var data = new NetworkInputData();
-        data.InputVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        input.Set(data);
-    }
-
     public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
     {
         if (runner.IsServer)
         {
-            var position = Vector3.zero;
+            var position = new Vector3(0, 1, 0);
             var networkObject = runner.Spawn(playerPrefab, position, Quaternion.identity, player);
-
             _spawnedCharacters.Add(player, networkObject);
         }
     }
 
     public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
     {
-        if (!_spawnedCharacters.TryGetValue(player, out var playerObject)) return;
+        if (_spawnedCharacters.TryGetValue(player, out var playerObject))
+        {
+            runner.Despawn(playerObject);
+            _spawnedCharacters.Remove(player);
+        }
+    }
 
-        runner.Despawn(playerObject);
-        _spawnedCharacters.Remove(player);
+    public void OnInput(NetworkRunner runner, NetworkInput input)
+    {
+        var data = new NetworkInputData();
+        data.InputVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        input.Set(data);
     }
 
     #region Unused Fusion Callbacks
-    public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input)
-    {
-
-    }
-
-    public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
-    {
-
-    }
-
-    public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
-    {
-
-    }
-
-    public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress)
-    {
-
-    }
-
-    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, ArraySegment<byte> data)
-    {
-
-    }
-
-    public void OnSceneLoadDone(NetworkRunner runner)
-    {
-
-    }
-
-    public void OnSceneLoadStart(NetworkRunner runner)
-    {
-
-    }
-
-    public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList)
-    {
-
-    }
-
-    public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason)
-    {
-
-    }
-
-    public void OnConnectedToServer(NetworkRunner runner)
-    {
-
-    }
-
-    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason)
-    {
-
-    }
-
-    public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token)
-    {
-
-    }
-
-    public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data)
-    {
-
-    }
-
-    public void OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason)
-    {
-
-    }
-
-    public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken)
-    {
-
-    }
-
-
-    public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message)
-    {
-
-    }
-
+    public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input) { }
+    public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
+    public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) { }
+    public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress) { }
+    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, ArraySegment<byte> data) { }
+    public void OnSceneLoadDone(NetworkRunner runner) { }
+    public void OnSceneLoadStart(NetworkRunner runner) { }
+    public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList) { }
+    public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason) { }
+    public void OnConnectedToServer(NetworkRunner runner) { }
+    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason) { }
+    public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token) { }
+    public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data) { }
+    public void OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason) { }
+    public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken) { }
+    public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message) { }
     #endregion
 }
